9c6bae5945e8c2106fb7066273731c60a7c82d9d
张亚飞 <672233580@qq.com>
2020-01-03 22:56:50 +0800

    1
    1

--- 第七天/04-swiper插件的使用/js/index.js
+++ 第七天/04-swiper插件的使用/js/index.js
@@ -15,4 +15,17 @@ window.addEventListener('load', function() {
             prevEl: '.swiper-button-prev',
         },
     });
+	
+	Swiper.on('slideChangeTransitionEnd', function () {
+	
+	 var imgurl = $('.swiper-slide-active img').attr("src");  
+	
+	var txt = $('.swiper-slide-active').html();  
+	
+	console.log("内容："+txt+"===索引值："+Swiper.activeIndex+"===图片地址："+imgurl);
+	
+	});
+	
+
+	
 })
